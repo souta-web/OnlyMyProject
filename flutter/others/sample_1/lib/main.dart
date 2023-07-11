9801ed0bf25db3f4811a78f6cf3bfd162ec0d9bd
souta-web <souta200319@gmail.com>
2023-07-12 01:27:32 +0900

    aa

--- flutter/others/sample_1/lib/main.dart
+++ flutter/others/sample_1/lib/main.dart
@@ -1,51 +1,84 @@
 import 'package:flutter/material.dart';
-import 'dart:math';
+import 'package:provider/provider.dart';
+import 'theme_helper.dart';
 
-import 'package:fl_chart/fl_chart.dart';
+// テーマ変更用の状態クラス
+class MyTheme extends ChangeNotifier {
+  ThemeData current = ThemeData.light();
+  bool _isDark = false;
 
-void main() {
-  runApp(const MyApp());
+  toggle() {
+    _isDark = !_isDark;
+    current = _isDark ? ThemeData.dark() : ThemeData.light();
+    notifyListeners();
+  }
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+void main() => runApp(MyApp());
 
+// MyApp ウイジェット
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,//デバッグモードバナーを非表示にする
-      title: 'Piechart',
-      theme: ThemeData(//アプリケーションのテーマカラーを決める
-        primarySwatch: Colors.blue,
+    return ChangeNotifierProvider(
+      create: (_) => MyTheme(),
+      child: Consumer<MyTheme>(
+        builder: (context, theme, _) {
+          return MaterialApp(
+            theme: theme.current,
+            home: MyHomePage(),
+          );
+        },
       ),
-      home: HomePage(),
     );
   }
 }
 
-class HomePage extends StatelessWidget {
-  HomePage({Key? key}) : super(key: key);
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePage createState() => _MyHomePage();
+}
+
+// MyHomePage ウイジェット
+class _MyHomePage extends State<MyHomePage> {
+  final String title = "Example";
+  bool isButtonToggled = false;
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(//appbarのある画面を作るという宣言
+    return Scaffold(
       appBar: AppBar(
-        title: const Text('Piechart'),
+        title: Text(title),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(30),
-        child: PieChart(PieChartData(
-            centerSpaceRadius: 5,
-            borderData: FlBorderData(show: false),
-            sectionsSpace: 2,
-            sections: [
-              PieChartSectionData(value: 35, color: Colors.purple, radius: 100),
-              PieChartSectionData(value: 40, color: Colors.amber, radius: 100),
-              PieChartSectionData(value: 55, color: Colors.green, radius: 100),
-              PieChartSectionData(value: 70, color: Colors.orange, radius: 100),
-            ])
-            )
-          ),
-        );
-    }
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children:[
+            Text('テーマ切替テスト'),
+            themeChangeButton(isButtonToggled, (bool newValue) {
+              setState(() {
+                isButtonToggled = newValue;
+              });
+            }),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed:  () {
+          Provider.of<MyTheme>(context, listen: false).toggle();
+        },
+        child: Icon(Icons.autorenew),
+      ),
+
+    );
+  }
+}
+
+Widget themeChangeButton(bool _isToggled, Function(bool) onPressed) {
+  return ElevatedButton(
+    onPressed: () {
+      _isToggled = !_isToggled;
+    },
+    child: Text(_isToggled ? 'ON' : 'OFF'),
+  );
 }
---
+++ flutter/others/sample_1/lib/theme_helper.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+class MyTheme extends ChangeNotifier {
+  ThemeData current = ThemeData.light();
+  bool _isDark = false;
+
+  toggle() {
+    _isDark = !_isDark;
+    current = _isDark ? ThemeData.dark() : ThemeData.light();
+    notifyListeners();
+  }
+}
+
+
