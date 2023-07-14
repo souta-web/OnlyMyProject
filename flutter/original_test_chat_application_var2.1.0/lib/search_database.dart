c9e154a3e89e08f6b76a4c7f42ce886123c559f9
ayataka-hhth21170020 <114968067+ayataka-hhth21170020@users.noreply.github.com>
2023-07-14 14:03:05 +0900

    大文字小文字を区別できるように実装

--- flutter/original_test_chat_application_var2.1.0/lib/search_database.dart
+++ flutter/original_test_chat_application_var2.1.0/lib/search_database.dart
@@ -1,33 +1,43 @@
 import 'database_helper.dart';
 
 class SearchDatabase {
+  // 検索メソッド
   Future<void> search(String keyword) async {
     // データベースヘルパーのインスタンスを生成
-    DatabaseHelper dbHelper = DatabaseHelper.instance;
+    final DatabaseHelper dbHelper = DatabaseHelper.instance;
+
+    // キーワードを小文字に変換
+    keyword = keyword.toLowerCase();
 
     // データベースからチャットテーブルの一致するレコードを検索
     List<Map<String, dynamic>> chatRecords =
         await dbHelper.queryAllRows_chat_table();
     List<Map<String, dynamic>> matchedChatRecords = chatRecords
         .where((record) =>
-            record[DatabaseHelper.columnChatId].toString().contains(keyword) ||
+            record[DatabaseHelper.columnChatId].toString().toLowerCase().contains(keyword) ||
             record[DatabaseHelper.columnChatSender]
                 .toString()
+                .toLowerCase()
                 .contains(keyword) ||
             record[DatabaseHelper.columnChatTodo]
                 .toString()
+                .toLowerCase()
                 .contains(keyword) ||
             record[DatabaseHelper.columnChatTodofinish]
                 .toString()
+                .toLowerCase()
                 .contains(keyword) ||
             record[DatabaseHelper.columnChatMessage]
                 .toString()
+                .toLowerCase()
                 .contains(keyword) ||
             record[DatabaseHelper.columnChatTime]
                 .toString()
+                .toLowerCase()
                 .contains(keyword) ||
             record[DatabaseHelper.columnChatChannel]
                 .toString()
+                .toLowerCase()
                 .contains(keyword))
         .toList();
 
@@ -38,42 +48,55 @@ class SearchDatabase {
         .where((record) =>
             record[DatabaseHelper.columnActionId]
                 .toString()
+                .toLowerCase()
                 .contains(keyword) ||
             record[DatabaseHelper.columnActionName]
                 .toString()
+                .toLowerCase()
                 .contains(keyword) ||
             record[DatabaseHelper.columnActionStart]
                 .toString()
+                .toLowerCase()
                 .contains(keyword) ||
             record[DatabaseHelper.columnActionEnd]
                 .toString()
+                .toLowerCase()
                 .contains(keyword) ||
             record[DatabaseHelper.columnActionDuration]
                 .toString()
+                .toLowerCase()
                 .contains(keyword) ||
             record[DatabaseHelper.columnActionMessage]
                 .toString()
+                .toLowerCase()
                 .contains(keyword) ||
             record[DatabaseHelper.columnActionMedia]
                 .toString()
+                .toLowerCase()
                 .contains(keyword) ||
             record[DatabaseHelper.columnActionNotes]
                 .toString()
+                .toLowerCase()
                 .contains(keyword) ||
             record[DatabaseHelper.columnActionScore]
                 .toString()
+                .toLowerCase()
                 .contains(keyword) ||
             record[DatabaseHelper.columnActionState]
                 .toString()
+                .toLowerCase()
                 .contains(keyword) ||
             record[DatabaseHelper.columnActionPlace]
                 .toString()
+                .toLowerCase()
                 .contains(keyword) ||
             record[DatabaseHelper.columnActionMainTag]
                 .toString()
+                .toLowerCase()
                 .contains(keyword) ||
             record[DatabaseHelper.columnActionSubTag]
                 .toString()
+                .toLowerCase()
                 .contains(keyword))
         .toList();
 
@@ -82,12 +105,14 @@ class SearchDatabase {
         await dbHelper.queryAllRows_tag_table();
     List<Map<String, dynamic>> matchedTagRecords = tagRecords
         .where((record) =>
-            record[DatabaseHelper.columnTagName].toString().contains(keyword) ||
+            record[DatabaseHelper.columnTagName].toString().toLowerCase().contains(keyword) ||
             record[DatabaseHelper.columnTagColor]
                 .toString()
+                .toLowerCase()
                 .contains(keyword) ||
             record[DatabaseHelper.columnTagRegisteredActionName]
                 .toString()
+                .toLowerCase()
                 .contains(keyword))
         .toList();
 
