01bbdc0f06c810cd49f01a9e6e36f061ea63a9c1
souta-web <souta200319@gmail.com>
2023-08-10 21:39:39 +0900

    福山さん吹き出し修正用プログラム作成

---
+++ flutter/hukuyama_fukidashi_sankou/lib/chat_fukidashi.dart
@@ -0,0 +1,109 @@
+import 'package:flutter/material.dart';
+
+//メッセージ送信吹き出し
+class ChatMessageSend extends StatelessWidget {
+  final String send;
+  ChatMessageSend({required this.send});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        child: ListView(
+          //画面全体の周りの余白
+          padding: const EdgeInsets.all(20.0),
+          children: [
+            Expanded(
+              child: Column(
+                children: [
+                  Padding(
+                    //吹き出し下の余白
+                    padding: EdgeInsets.only(bottom: 18.0),
+                    child: Align(
+                      //右側に寄せる
+                      alignment: Alignment.centerRight,
+                      child: Container(
+                        //角の丸みの設定
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.only(
+                            topRight: Radius.circular(10),
+                            topLeft: Radius.circular(10),
+                            bottomLeft: Radius.circular(10),
+                          ),
+                          color: Color.fromARGB(255, 255, 149, 21),
+                        ),
+                        child: Padding(
+                          //文字と吹き出しの間の余白
+                          padding: EdgeInsets.all(15.0),
+                          child: Text(
+                            send,
+                            style: TextStyle(fontSize: 13),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+    );
+  }
+}
+
+//メッセージ返信吹き出し
+//childが6つ使われているけど、本来は１つのウィジェットに対してそこまで使うものではないので単純化してほしい
+class ChatMessageReply extends StatelessWidget {
+  final String reply;
+  ChatMessageReply({required this.reply});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold( //61～63にかけてscaffold,container,listview書いている部分は、return Containerのみでも問題ないかも(辻)
+      body: Container(
+        child: ListView( //child1つめ(辻)
+          //画面全体の周りの余白
+          padding: const EdgeInsets.all(20.0),
+          children: [
+            Expanded(
+              child: Column(//child2つめ(辻)
+                children: [
+                  Padding(
+                    //吹き出し下の余白
+                    padding: EdgeInsets.only(bottom: 28.0),
+                    child: Align(//child3つめ(辻)
+                      //左側に寄せる
+                      alignment: Alignment.centerLeft,
+                      child: Container(//child4つめ(辻)
+                        //角の丸みの設定
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.only(
+                            topRight: Radius.circular(10),
+                            topLeft: Radius.circular(10),
+                            bottomRight: Radius.circular(10),
+                          ),
+                          color: Color.fromARGB(255, 189, 187, 184),
+                        ),
+                        child: Padding(//child5つめ(辻)
+                          //文字と吹き出しの間の余白
+                          padding: EdgeInsets.all(15.0),
+                          child: Text(//child6つめ(辻)
+                            reply,
+                            style: TextStyle(fontSize: 13),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+
+
--- flutter/hukuyama_fukidashi_sankou/lib/main.dart
+++ flutter/hukuyama_fukidashi_sankou/lib/main.dart
@@ -1,12 +1,13 @@
 import 'package:flutter/material.dart';
+import 'chat_fukidashi.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
+//MyAppの中身は無視でよい
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
@@ -16,36 +17,73 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-  final String title;
-
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
+//実際に表示されている画面以下
+//_messagesに吹き出しのクラスを格納して画面に表示されるように作ってほしい
 class _MyHomePageState extends State<MyHomePage> {
-
+  //
+  final List<dynamic> _messages = [ChatMessage(text: "aaaa",isSentByUser: true),ChatMessage(text: "testdayo", isSentByUser: false)];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text('Chat Screen'),
+        backgroundColor: Colors.blue,
       ),
-      body: Center(
-        
+      body: Column(
+        children: [
+          Expanded(
+            child: ListView.builder(
+              itemCount: _messages.length, //_messagesの中身の数を取得する
+              itemBuilder: (context, index) { //47,48行目で画面描画用の吹き出し作成
+                return _messages[index];
+              },
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+//辻が作った吹き出し
+class ChatMessage extends StatelessWidget {
+  // チャットメッセージのテキスト
+  final String text;
+  // 送信者がユーザー自身かどうかのフラグ
+  final bool isSentByUser;
+
+  //クラスを呼び出すときに引数を必要とする(辻)
+  ChatMessage({required this.text, required this.isSentByUser});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      // 送信者に応じてメッセージの位置を調整する
+      alignment: isSentByUser ? Alignment.centerRight : Alignment.centerLeft,
+      child: Container(
+        margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 10.0),
+        padding: EdgeInsets.all(10.0),
+        decoration: BoxDecoration(
+          // 送信者に応じてメッセージの背景色を設定する
+          color: isSentByUser ? Colors.blue : Colors.grey[300],
+          // 角丸のボーダーを適用する
+          borderRadius: BorderRadius.circular(20.0),
+        ),
+        child: Text(
+          text,
+          style: TextStyle(fontSize: 16.0),
+        ),
       ),
     );
   }
