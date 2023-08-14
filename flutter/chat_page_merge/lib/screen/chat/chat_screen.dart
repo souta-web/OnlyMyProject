e72a748178967a984d563ced3e51845e4b5d7161
souta-web <souta200319@gmail.com>
2023-08-14 22:41:50 +0900

    aa

--- flutter/chat_page_merge/lib/screen/chat/chat_screen.dart
+++ flutter/chat_page_merge/lib/screen/chat/chat_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import '/widget/chat_fukidashi.dart';
 
 class ChatScreenWidget extends StatelessWidget {
   @override
@@ -6,21 +7,93 @@ class ChatScreenWidget extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text('Chat'),
-        automaticallyImplyLeading: false, // バックボタンを非表示にする
-        actions: [  
+        //左上のアイコン
+        leading: IconButton(
+          icon: CircleAvatar(
+            backgroundImage: AssetImage('assets/images/account_icon.png'),
+            backgroundColor: Colors.transparent, // 背景色
+            radius: 25,
+          ),
+          onPressed: () {},
+        ),
+        actions: [
           IconButton(
-            icon: Icon(Icons.settings),
+            icon: Icon(Icons.more_vert),
             onPressed: () {
-              Navigator.pushNamed(context, '/config');//routeに追加したconfigに遷移
+              Navigator.pushNamed(context, '/config'); //routeに追加したconfigに遷移
             },
           ),
         ],
       ),
-      ///記述範囲
-      body: Center(
-        child: Text('Chat Screen'),
-      ),
-      ///記述範囲
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          Container(
+            color: Color.fromARGB(255, 103, 100, 100),
+            child: Column(
+              children: [
+                Form(
+                  child: Row(
+                    crossAxisAlignment: CrossAxisAlignment.end,
+                    children: [
+                      IconButton(
+                        icon: Icon(Icons.textsms),
+                        color: Colors.white,
+                        onPressed: () {},
+                      ),
+                      IconButton(
+                        icon: Icon(Icons.add),
+                        color: Colors.white,
+                        onPressed: () {},
+                      ),
+                      Row(
+                        
+                      ),
+                      Flexible(
+                        child: Container(
+                          padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                          margin: const EdgeInsets.symmetric(vertical: 5.0),
+                          height: 40.0,
+                          decoration: BoxDecoration(
+                            color: Color.fromARGB(255, 222, 216, 216),
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                          child: TextFormField(
+                            keyboardType: TextInputType.multiline, //複数行のテキスト入力
+                            maxLines: 5,
+                            minLines: 1,
+                            cursorColor: Color.fromARGB(255, 75, 67, 93),
+                            decoration: InputDecoration(
+                              border: InputBorder.none,
+                              hintText: 'メッセージを入力してください',
+                              //hintTextの位置
+                              contentPadding: EdgeInsets.symmetric(
+                                vertical: 11.0,
+                              )
+                            ), 
+                          ),
+                        )
+                      ),
+                      Material(
+                        color: Color.fromARGB(255, 103, 100, 100),
+                        child: Center(
+                          child: Ink(
+                            child: IconButton(
+                              icon: Icon(Icons.send),
+                              color: Colors.white,
+                              onPressed: () {},
+                            ),
+                          ),
+                        ),
+                      )
+                    ]
+                  )
+                ),
+              ]
+            )
+          ),
+        ],
+      )
     );
   }
 }
