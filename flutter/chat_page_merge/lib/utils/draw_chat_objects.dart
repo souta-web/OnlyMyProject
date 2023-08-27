1f3692e2c6b71eb7e337f41d637d4f2ecc271f60
ayataka-hhth21170020 <114968067+ayataka-hhth21170020@users.noreply.github.com>
2023-08-28 01:42:22 +0900

    作業中zzzzzzzzzzzzzzzzzzzzzzzzz

---
+++ flutter/chat_page_merge/lib/utils/draw_chat_objects.dart
@@ -0,0 +1,10 @@
+import '/widget/chat_fukidashi.dart';
+import '/widget/chat_todo.dart';
+
+// トグルボタンの状態によってオブジェクトを表示する
+class DrawChatObjects {
+
+  void drawChatObjects(bool isTodo) {
+    
+  }
+}
