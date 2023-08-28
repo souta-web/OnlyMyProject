42bffe49265fbc7bbdd949486574f3335f00f86b
souta-web <souta200319@gmail.com>
2023-08-28 23:36:09 +0900

    タスク説明とプロジェクト追加

---
+++ flutter/chat_page_merge/lib/utils/register_chat_table.dart
@@ -0,0 +1,58 @@
+import 'database_helper.dart';
+
+//動作未チェック
+
+class RegisterChatTable{
+  final int? chatId; //idはデータ登録時に自動で割り当てられるため、引数で渡す必要は基本的にはない
+  final String? chatSender;
+  final String? chatTodo;
+  final int? chatTodofinish;
+  final String? chatMessage;
+  final String? chatTime;
+  final String? chatChannel;
+  final int? chatActionId;
+
+  RegisterChatTable ({this.chatId,
+                    this.chatSender,
+                    this.chatTodo,
+                    this.chatTodofinish,
+                    this.chatMessage,
+                    this.chatTime,
+                    this.chatChannel,
+                    this.chatActionId
+                    });
+
+  void registerChatTableFunc() async {
+    // row to insert
+    //データベースに登録
+    print("これからデータベース登録");
+    final DatabaseHelper dbHelper = DatabaseHelper.instance;
+    Map<String, dynamic> row = {
+      DatabaseHelper.columnChatId : chatId,
+      DatabaseHelper.columnChatSender : chatSender,
+      DatabaseHelper.columnChatTodo : chatTodo,
+      DatabaseHelper.columnChatTodofinish : chatTodofinish,
+      DatabaseHelper.columnChatMessage : chatMessage,
+      DatabaseHelper.columnChatTime : chatTime,
+      DatabaseHelper.columnChatChannel : chatChannel,
+      DatabaseHelper.columnChatActionId : chatActionId,
+    };
+
+    await dbHelper.insert_chat_table(row);
+
+    //↓デバッグ用のデータ表示プログラム
+    final allRows = await dbHelper.queryAllRows_chat_table();
+    print('全てのデータを照会しました。');
+    allRows.forEach(print);
+  }
+}
+
+/*
+次のような形で呼び出すことができる
+  RegisterChatTable registerChatTable = RegisterChatTable( //インスタンス化、引数渡し
+    chatSender: 'John',
+    chatMessage: 'Hello!',
+  );
+
+  registerChatTable.registerChatTableFunc(); //実際にデータベース登録
+*/
---
+++ flutter/chat_page_merge/lib/widget/chat_fukidashi.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+// 辻が作った吹き出し
+class ChatMessage extends StatelessWidget {
+  // チャットメッセージのテキスト
+  final String text;
+  // 送信者がユーザー自身かどうかのフラグ
+  final bool isSentByUser;
+
+  //クラスを呼び出すときに引数を必要とする(辻)
+  ChatMessage({required this.text, required this.isSentByUser,});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      // 送信者に応じてメッセージの位置を調整する
+      alignment: isSentByUser ? Alignment.centerRight : Alignment.centerLeft,
+      child: Container(
+        margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 10.0),
+        padding: EdgeInsets.all(10.0),
+        decoration: BoxDecoration(
+          // 送信者に応じてメッセージの背景色を設定する
+          color: isSentByUser ? Color.fromARGB(255, 255, 149, 21) : Color.fromARGB(255, 189, 187, 184),
+          // 角丸のボーダーを適用する
+          borderRadius: isSentByUser ?
+          BorderRadius.only(
+            topRight: Radius.circular(10),
+            topLeft: Radius.circular(10),
+            bottomLeft: Radius.circular(10),
+          ) : 
+          BorderRadius.only(
+            topRight: Radius.circular(10),
+            topLeft: Radius.circular(10),
+            bottomRight: Radius.circular(10),
+          )
+        ),
+        child: Text(
+          text,
+          style: TextStyle(fontSize: 16.0),
+        ),
+      ),
+    );
+  }
+}
