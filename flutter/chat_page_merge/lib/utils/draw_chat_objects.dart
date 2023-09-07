f140aca9f0d391fe6fc821f7f862dcf74c72fe86
ayataka-hhth21170020 <114968067+ayataka-hhth21170020@users.noreply.github.com>
2023-09-07 15:09:13 +0900

    インデント整えた

--- flutter/chat_page_merge/lib/utils/draw_chat_objects.dart
+++ flutter/chat_page_merge/lib/utils/draw_chat_objects.dart
@@ -9,8 +9,14 @@ import 'register_action_table.dart';
 class DrawChatObjects {
   // チャットオブジェクトを表示する
   //受け取れる引数増やせば、アプリ再起動時の履歴復元にも使えるので
-  Widget drawChatObjects({required bool isTodo, required String chatText, required bool isUser, required String mainTag, required String startTime, required bool isActionFinished,}) {
-    
+  Widget drawChatObjects({
+    required bool isTodo,
+    required String chatText,
+    required bool isUser,
+    required String mainTag,
+    required String startTime,
+    required bool isActionFinished,
+  }) {
     Widget chatWidget; // Widgetを格納する変数
 
     if (isTodo) {
@@ -19,30 +25,32 @@ class DrawChatObjects {
           title: chatText,
           isSentByUser: isUser,
           mainTag: mainTag,
-          startTime: startTime, //チャットオブジェクトを表示することが目的の関数なので、日時を取得してそれを表示させるのはふさわしくない。引数で受け取るようにする。(辻)
+          startTime:
+              startTime, //チャットオブジェクトを表示することが目的の関数なので、日時を取得してそれを表示させるのはふさわしくない。引数で受け取るようにする。(辻)
           actionFinished: isActionFinished);
       chatWidget = message; // chatWidgetにメッセージを代入
-
-    }else{
+    } else {
       chatWidget = Container(); // デフォルトの値を代入
-      if(chatText.isNotEmpty){
+      if (chatText.isNotEmpty) {
         if (isUser) {
-        ChatMessage message = ChatMessage(text: chatText, isSentByUser: isUser); // 応答側のメッセージ
-        chatWidget = message; // chatWidgetにメッセージを代入
-        }else{
-          ChatMessage message = ChatMessage(text: chatText, isSentByUser: isUser); // 返答側のメッセージ
+          ChatMessage message =
+              ChatMessage(text: chatText, isSentByUser: isUser); // 応答側のメッセージ
+          chatWidget = message; // chatWidgetにメッセージを代入
+        } else {
+          ChatMessage message =
+              ChatMessage(text: chatText, isSentByUser: isUser); // 返答側のメッセージ
           chatWidget = message; // chatWidgetにメッセージを代入
         }
       }
     }
-    return chatWidget;  // chatWidgetを返す
+    return chatWidget; // chatWidgetを返す
     //↓チャットオブジェクトを返す関数の本筋と直接関係のない処理は書き込まないほうが良い。
     //関数は基本的に1つの機能のみを持たせる。今回の場合はチャットオブジェクトを作ること＆コントローラーのクリアの2つの機能が含まれることになるから良くない。
   }
 
   // 送信ボタンが押されたときに呼び出される
   //引数にisUser追加(辻)
-  sendButtonPressed(String chatText, bool isTodo, 
+  sendButtonPressed(String chatText, bool isTodo,
       TextEditingController controller, bool isUser) {
     controller.clear();
     if (chatText.isNotEmpty) {
@@ -65,6 +73,13 @@ class DrawChatObjects {
     }
     // 吹き出し及びアクションの表示
     // 吹き出しクラスの引数を受け取れるように変更
-    return drawChatObjects(isTodo: isTodo, chatText: chatText, isUser: isUser, mainTag: '#趣味', startTime: DateTime.now().toString(), isActionFinished: false,);
+    return drawChatObjects(
+      isTodo: isTodo,
+      chatText: chatText,
+      isUser: isUser,
+      mainTag: '#趣味',
+      startTime: DateTime.now().toString(),
+      isActionFinished: false,
+    );
   }
 }
