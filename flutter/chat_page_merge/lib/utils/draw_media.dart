64692cf5af25a982470bfb2dfa671c2d814156ec
ayataka-hhth21170020 <114968067+ayataka-hhth21170020@users.noreply.github.com>
2023-09-19 16:10:14 +0900

    個別のカラムにデータが保存されるように修正

--- flutter/chat_page_merge/lib/utils/draw_media.dart
+++ flutter/chat_page_merge/lib/utils/draw_media.dart
@@ -8,32 +8,24 @@ class DrawMedia {
   final ImagePicker _imagePicker = ImagePicker(); // ImagePickerのインスタンス生成
 
   // 画像を選択してバイナリデータのリストを返す非同期メソッド
-  Future<List<Uint8List>> pickImages() async {
+  Future<void> pickImages() async {
     final List<XFile>? pickedFiles =
         await _imagePicker.pickMultiImage(); // ユーザーに複数の画像を選択させる
-    final List<Uint8List> imageList = []; // 選択された画像のバイナリーデータを格納するリスト
 
     // ファイルが選択され、リストが空でない時の場合の処理
     if (pickedFiles != null && pickedFiles.isNotEmpty) {
-      print(pickedFiles);
-      for (XFile file in pickedFiles) {
+      for (int i = 0; i < pickedFiles.length; i++) {
+        final XFile file = pickedFiles[i];
         final Uint8List bytes =
             await io.File(file.path).readAsBytes(); // ファイルを読み込んでバイナリーデータに変換
-        imageList.add(Uint8List.fromList(bytes)); // バイナリーデータをリストに追加
+        RegisterMediaTable registerMediaTable = RegisterMediaTable(
+          media1: i == 0 ? Uint8List.fromList(bytes) : null,
+          media2: i == 1 ? Uint8List.fromList(bytes) : null,
+          media3: i == 2 ? Uint8List.fromList(bytes) : null,
+          media4: i == 3 ? Uint8List.fromList(bytes) : null,
+        );
+        registerMediaTable.registerMediaTableFunc();
       }
     }
-
-    // 画像が1枚以上選択されている場合にのみデータベースに登録
-    if (imageList.isNotEmpty) {
-      RegisterMediaTable registerMediaTable = RegisterMediaTable(
-        media1: imageList.length > 0 ? imageList[0] : null,
-        media2: imageList.length > 1 ? imageList[1] : null,
-        media3: imageList.length > 2 ? imageList[2] : null,
-        media4: imageList.length > 3 ? imageList[3] : null,
-      );
-      registerMediaTable.registerMediaTableFunc();
-    }
-    print(imageList);
-    return imageList; // 選択された画像のバイナリーデータのリストを返す
   }
 }
