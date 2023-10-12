2bf3c503fbed85aaa0a1247e439dff0c6f491401
ib320 <137030259+ib320@users.noreply.github.com>
2023-10-13 04:13:42 +0900

    4
    4

--- flutter/chat_page_work/lib/screen/data/data_screen.dart
+++ flutter/chat_page_work/lib/screen/data/data_screen.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:nosato_chat_page_work/screen/data/line_chart_widget.dart';
+//import 'package:nosato_chat_page_work/screen/data/pi_chart_widget.dart';
+//import 'package:nosato_chat_page_work/screen/data/donuts_chart_widget.dart';
+//import 'package:nosato_chat_page_work/screen/data/ddonut_chart_widget.dart';
 
 class DataScreenWidget extends StatelessWidget {
   @override
@@ -6,23 +10,26 @@ class DataScreenWidget extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text('Data'),
-        automaticallyImplyLeading: false, // バックボタンを非表示にする
+        automaticallyImplyLeading: false,
         actions: [
           IconButton(
             icon: Icon(Icons.settings),
             onPressed: () {
-              Navigator.pushNamed(context, '/config'); //routeに追加したconfigに遷移
+              Navigator.pushNamed(context, '/config');
             },
           ),
         ],
       ),
-
-      ///記述範囲
-      body: Center(
-        child: Text('Data Screen'),
+      body: Column(
+        children: [
+          // 他のデータ表示ウィジェット
+          //各グラフを表示
+          LineChartWidget(), //折れ線グラフ
+          //DynamicPieChart(), //円フラフ
+          //DonutChartSample(), //ドーナツ状の円グラフ
+          //DDonutChartSample(), //ドーナツ状の時刻グラフ
+        ],
       ),
-
-      ///記述範囲
     );
   }
 }
