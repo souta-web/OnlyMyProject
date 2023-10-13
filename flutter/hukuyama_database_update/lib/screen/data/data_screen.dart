30a33dad4eb9cc0fb749ef6f5172d7dc61dfa2b9
FukuChisa <hhth21170069@class.hht.ac.jp>
2023-10-13 13:36:57 +0900

    グラフ表示

--- flutter/hukuyama_database_update/lib/screen/data/data_screen.dart
+++ flutter/hukuyama_database_update/lib/screen/data/data_screen.dart
@@ -1,6 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:fl_chart/fl_chart.dart';
 
 class DataScreenWidget extends StatelessWidget {
+
+  // 棒グラフの棒の横幅
+  static double barWidth = 20.0;
+  // グラフタイトルのラベル書式
+  final TextStyle labelStyle = TextStyle(fontSize: 16, fontWeight: FontWeight.w800);
+
+  // ダミーデータ
+  final timedate = <double>[30, 25, 20, 15, 10];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,11 +26,47 @@ class DataScreenWidget extends StatelessWidget {
           ),
         ],
       ),
-      ///記述範囲
       body: Center(
-        child: Text('Data Screen'),
+        child: BarChart(
+            //vertical: false,
+          BarChartData(
+            // 棒グラフの位置
+            alignment: BarChartAlignment.spaceEvenly,
+            
+            // グリッド線を非表示にする
+            gridData: FlGridData(
+              show: false, 
+            ),
+            // 枠線を非表示にする
+            borderData: FlBorderData(
+              show: false,
+            ),
+            /* 左右のラベルを非表示にする
+            titlesData: FlTitlesData(
+              leftTitles: SideTitles(show: false),
+              rightTitles: SideTitles(show: false),
+            ),*/
+
+            barGroups: [
+              BarChartGroupData(x: 2, barRods: [
+                BarChartRodData(toY: timedate[0], width: barWidth, color: Colors.blue),
+              ]),
+              BarChartGroupData(x: 3, barRods: [
+                BarChartRodData(toY: timedate[1], width: barWidth, color: Colors.pink),
+              ]),
+              BarChartGroupData(x: 4, barRods: [
+                BarChartRodData(toY: timedate[2], width: barWidth, color: Colors.orange),
+              ]),
+              BarChartGroupData(x: 5, barRods: [
+                BarChartRodData(toY: timedate[3], width: barWidth, color: Colors.green),
+              ]),
+              BarChartGroupData(x: 6, barRods: [
+                BarChartRodData(toY: timedate[4], width: barWidth, color: Colors.grey),
+              ]),
+            ]
+          ),
+        ),
       ),
-      ///記述範囲
     );
   }
 }
